a0de5b3a14ad826070a32f79f6d30783e9d493a4
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2025-11-30 21:55:17 +0530

    Add debug tracing

--- main.c
+++ main.c
@@ -15,7 +15,8 @@ int main() {
 
   writeChunk(&chunk, OP_RETURN, 123);
 
-  disassembleChunk(&chunk, "test chunk");
+  // disassembleChunk(&chunk, "test chunk");
+  interpret(&chunk);
 
   freeVM();
   freeChunk(&chunk);
